ac87c3e297a07567ca8b0fb6ffe38ff8a156237a
td2deshan <thilanga252deshan@gmail.com>
2021-02-01 23:04:23 +0530

    add heroku config files

--- main.py
+++ main.py
@@ -30,6 +30,6 @@ def configure():
 
 configure()
 
-
-if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+#
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="127.0.0.1", port=5000)
